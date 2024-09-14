4a2b90a51d1fe8e07a912990e6be8bf4f3783a4f
pranitbhagat1 <47513382+pranitbhagat1@users.noreply.github.com>
2024-09-14 19:22:00 +0530

    Initial commit

---
+++ lib/constants/theme_constants.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+Color pageBackground = Color(0xffdbecf4);
+// Color pageBackground = Color(0xffd9ecf3);
+Color splashCircle = Color(0xffedfbfd);
+
+Color homeGradient1 = Color(0xff695ee1);
+Color homeGradient2 = Color(0xff4e3fc0);
+Color homeGradient3 = Color(0xff432eae);
+Color homeGradient4 = Color(0xff432eb0);
+
+Color cardHome = Color(0xff5450d4);
+
+Color selectedDate = Color(0xff402fa5);
+
+Color activeNavigationIndex = Color(0xff698ff0);
